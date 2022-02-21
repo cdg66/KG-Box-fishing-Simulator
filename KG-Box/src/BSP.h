f5d316f790987287043993d7a0999f819e3be5da
cdg66 <claudecdg66@icloud.com>
2022-02-20 21:40:59 -0500

    add bsp

---
+++ KG-Box/src/BSP.h
@@ -0,0 +1,42 @@
+
+#ifndef BSP_H
+#define BSP_H
+
+#include "Arduino.h"
+
+
+#define ACC_X       A0
+#define ACC_Y       A1
+#define ACC_Z       A2
+
+#define JSTICK_A1   A3 
+#define JSTICK_A2   A4
+#define JSTICK_BTN  23
+
+#define ENC_A       2
+#define ENC_B       3
+
+#define A1          21
+#define B1          27
+#define C1          29
+#define D1          31
+#define E1          33
+#define F1          35
+#define G1          37
+#define A2          39
+#define B2          41
+#define C2          43
+#define D2          45
+#define E2          47
+#define F2          49
+#define G2          51
+#define SEGON       0
+
+#define BTN1        22
+#define BTN2        24
+#define BTN3        26
+#define BTN4        28
+ 
+#define Moteur      29 // not fixed
+
+#endif
