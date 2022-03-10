6da799e1fc71fbf68125ff20c6f22d7773c98dde
cdg66 <claudecdg66@icloud.com>
2022-03-10 12:41:57 -0500

    update réception BCD devient la vraie valeur

--- KG-Box/src/main.cpp
+++ KG-Box/src/main.cpp
@@ -51,12 +51,15 @@ void loop() {
   SeptSegement* seg1 = new SeptSegement (sA1,sB1,C1,D1, E1, F1, G1,0,SEGON );
   SeptSegement* seg2 = new SeptSegement (sA2,B2,C2,D2, E2, F2, G2,0,SEGON );
   uint8_t segement_7 = 0;
+  uint8_t Diz = 0;
+  uint8_t unite = 0;
   bool mot = 0;
   Encoder* myEnc = new Encoder(ENC_A, ENC_B);
   Joystick* Joystick0 = new Joystick(JSTICK_A1, JSTICK_A2);
   int16_t Acc_X = 0;
   int16_t Acc_Y = 0;
   int16_t Acc_Z = 0;
+
   while (1)
   {
 
@@ -73,45 +76,18 @@ void loop() {
       Acc_Y = 0;
       Acc_Z = 0;
     }
-    //Joystick0->GetArray(Position);
 
-    /*Serial.print("Joystick X: ");
-    Serial.print( Joystick0->GetX());
-    Serial.print(" ,");
-
-    Serial.print("Joystick Y: ");
-    Serial.print( Joystick0->GetY());
-    Serial.print(" ,");
-
-    Serial.print("Bouton2: ");
-    Serial.print(BOUTONSTATE(BTN2));
-    Serial.print(" ,");
-
-    Serial.print("Bouton3: ");
-    Serial.print(BOUTONSTATE(BTN3));
-    Serial.print(" ,");
-
-    Serial.print("Bouton4: ");
-    Serial.print(BOUTONSTATE(BTN4));
-    Serial.print(" ,");
-
-    Serial.print("Bouton5: ");
-    Serial.print(BOUTONSTATE(JSTICK_BTN));
-    Serial.print(" ,");
-
-    Serial.print("Encodeur: ");
-    Serial.print(myEnc->read());
-    Serial.print(" ,");
-    Serial.print(" \n");*/
-
+    
 
     sendMsg(Joystick0->GetX(), Joystick0->GetY(), BOUTONSTATE(BTN2), BOUTONSTATE(BTN3), BOUTONSTATE(BTN4), BOUTONSTATE(JSTICK_BTN), Acc_X, Acc_Y, Acc_Z, myEnc->read());
     if (Serial.available() )
     {
-      Serial.print("Message Recu \n");
+      //Serial.print("Message Recu \n");
       readMsg(segement_7, mot);
-      seg1->write(segement_7);
-      seg2->write((segement_7>>4));
+      unite = segement_7%10;
+      Diz = (segement_7 - unite)/10; 
+      seg2->write(unite);
+      seg1->write(Diz);
     }
     delay(10);
 
