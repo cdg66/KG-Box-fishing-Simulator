4d83d8ce253d4773fce9843143dc3819b112bf16
joel0duf <75927731+joel0duf@users.noreply.github.com>
2022-04-09 11:06:09 -0400

    derniers changements à Box

--- TestQT/projets2appli/box.h
+++ TestQT/projets2appli/box.h
@@ -11,11 +11,15 @@ private:
 	QBrush brush;
 	int X;
 	int Y;
+	int taille;
 public:
 	Box(QGraphicsItem* parent = 0);
 	~Box();
 	void setColor(QColor color);
+	void setTaille(int t);
 	void setCoord(int x, int y);
+	int getX();
+	int getY();
 
 };
 
